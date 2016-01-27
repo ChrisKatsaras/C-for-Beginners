5e9944fe387acbcbda3fb613b3336bec1b4b46fd
Christopher Katsaras <ckatsara@mail.uoguelph.ca>
2016-01-27 10:57:37 -0500

    Updated printingVariables.c

--- printingVariables.c
+++ printingVariables.c
@@ -4,11 +4,16 @@ int main()
 {
 	int num1;
 	int num2;
+	char firstInitial;
+	char secondInitial;
 
 	num1 = 4;
 	num2 = 9;
+	firstInitial = 'C';
+	secondInitial = 'K';	
 
 	printf("The first number is %d and the second number is %d\n",num1,num2);
+	printf("My initials are %c.%c\n",firstInitial,secondInitial);
 
 	return 0;
 }

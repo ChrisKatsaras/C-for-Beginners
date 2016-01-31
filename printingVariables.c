811f08f0859622e0a4bcf0c23967e3cae42a3eab
Christopher Katsaras <ckatsara@mail.uoguelph.ca>
2016-01-31 16:28:31 -0500

    Added double example to printingVariables.c

--- printingVariables.c
+++ printingVariables.c
@@ -4,15 +4,18 @@ int main()
 {
 	int num1;
 	int num2;
+	double doubleNum;
 	char firstInitial;
 	char secondInitial;
 
 	num1 = 4;
 	num2 = 9;
+	doubleNum = 6.6;
 	firstInitial = 'C';
 	secondInitial = 'K';	
 
 	printf("The first number is %d and the second number is %d\n",num1,num2);
+	printf("You can print numbers with decimals like %.1lf using the double data type\n",doubleNum);
 	printf("My initials are %c.%c\n",firstInitial,secondInitial);
 
 	return 0;
---
+++ simpleCalculations.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int num1;
+	int num2;
+	
+	num1 = 20;
+	num2 = 5;
+
+
+	
+
+	return 0;
+}

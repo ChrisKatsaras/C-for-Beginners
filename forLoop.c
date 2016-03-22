b3ca0fb945ebbf0328436c80ddf315089c9f0611
Christopher Katsaras <ckatsara@mail.uoguelph.ca>
2016-03-22 13:26:43 -0400

    Adding forLoop.c

---
+++ forLoop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+
+	for(i=0;i<10;i++) //I is equal to zero; Loop when i is less than 10; increment i by 1 after each loop
+	{
+		printf("%d\n",i); //Will print 0 to 9
+	}
+
+	return 0;
+}

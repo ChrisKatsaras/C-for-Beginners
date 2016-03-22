e360693d836eeb5f4b4011996454915cffe0c852
Christopher Katsaras <ckatsara@mail.uoguelph.ca>
2016-03-22 13:40:15 -0400

    Adding whileLoops.c

---
+++ whileLoops.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int i = 0;
+
+	printf("While Loop:\n");
+	while(i != 10) //Loop while i does NOT equal 10
+	{
+		printf("%d\n",i);
+		i++; //Increments i so we can eventually meet the exit condition
+	}
+
+	i = 0;
+
+	printf("Do while loop:\n");
+	do
+	{
+		printf("%d\n",i);
+		i++; //Increments i so we can eventually meet the exit condition
+
+	}while(i != 10);
+
+	return 0;
+}

61490edd6cb5e4fa5a97e11e589bd55742ee1aef
codereenter <72278028+codereenter@users.noreply.github.com>
2020-10-03 01:25:42 +0530

    Update helloWorld.c
    improving code for better reading

--- helloWorld.c
+++ helloWorld.c
@@ -1,5 +1,10 @@
+
+// Header file for input output functions
 #include <stdio.h>
 
+
+// main function - 
+// where the execution of program begins 
 int main()
 {
 	printf("Hello World!\n"); //Prints hello world and a newline
